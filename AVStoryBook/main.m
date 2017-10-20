a67bf14118a97edc4cde72f2347c875b1b13ae8c
Sanjay Shah <Sanjay Shah>
2017-10-20 14:39:41 -0700

    Initial Commit

---
+++ AVStoryBook/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVStoryBook
+//
+//  Created by Sanjay Shah on 2017-10-20.
+//  Copyright © 2017 Sanjay Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
